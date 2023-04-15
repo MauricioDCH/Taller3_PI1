1eb8a46e0802c8506f033b994e9f57c00e8ee74b
Mauricio Correa <mdcorreah@eafit.edu.co>
2023-04-14 19:51:19 -0500

    Ingreso de resto del taller 3

--- Taller3_PI1/moviesProject/moviesProject/urls.py
+++ Taller3_PI1/moviesProject/moviesProject/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", movieViews.home),
+    path("", movieViews.home, name="home"),
     path("news/", include('news.urls')),
 ]
 
